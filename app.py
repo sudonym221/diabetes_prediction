d01a43add7834c20073d77b24b2257ddace34c51
sudonym221 <deepjchetia007@gmail.com>
2025-07-04 15:58:35 +0530

    added port

--- app.py
+++ app.py
@@ -20,3 +20,9 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
